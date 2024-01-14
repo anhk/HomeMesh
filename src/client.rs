e526f9594a63d08ca6d8c165329e82874529658e
anhongkui <anhongkui@jd.com>
2024-01-14 11:01:17 +0000

    tcp connect

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,13 @@
+use std::net::TcpStream;
+
 use clap::Args;
+use log::info;
 
 #[derive(Args)]
 /// use client mode
 pub struct ClientOption {
     /// the address of server, it will connect ${server_addr}:${port} [default: <empty>],
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t = String::from("127.0.0.1"))]
     pub server_addr: String,
 
     /// the port of server, it will connect ${server_addr}:${port}
@@ -20,4 +23,9 @@ pub struct ClientOption {
     pub token: String,
 }
 
-pub fn connect(_option: &ClientOption) {}
+pub fn connect(option: &ClientOption) {
+    let addr = format!("{}:{}", option.server_addr, option.port);
+    info!("connect to {}", addr);
+    let stream = TcpStream::connect(addr).expect("connect failed");
+    stream.set_nodelay(true).expect("set nodelay failed");
+}
