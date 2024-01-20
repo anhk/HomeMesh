8aaae15b9efc1d061179590e179e21bc75ab41f9
anhongkui <anhongkui@jd.com>
2024-01-20 07:40:11 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use env_logger::{Builder, Target};
 mod args;
 mod client;
 mod server;
 mod tun;
+
+use env_logger::{Builder, Target};
+use log::info;
 use std::io::Read;
 
 #[tokio::main]
@@ -14,21 +16,20 @@ async fn main() {
         .filter(None, log::LevelFilter::Debug)
         .init();
     // env_logger::init();
+    match args::parse() {
+        args::Command::Server(option) => tokio::spawn(async move { server::listen(&option) }),
+        args::Command::Client(option) => tokio::spawn(async move { client::connect(&option) }),
+    };
 
     let mut data = [0u8; 2048];
     let mut tun = tun::alloc_tun().unwrap();
 
     while match tun.read(&mut data) {
         Ok(size) => {
-            println!("read {} from tun", size);
+            info!("read {} from tun", size);
             true
         }
         Err(_e) => false,
     } {}
-
-    match args::parse() {
-        args::Command::Server(option) => server::listen(&option),
-        args::Command::Client(option) => client::connect(&option),
-    }
     println!("Hello, world!");
 }
